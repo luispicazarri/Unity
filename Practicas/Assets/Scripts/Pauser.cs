05c06a4b1c6a5f320917d47bbc280a2a5850ced0
Luis Antonio Picazarri Quintana <luis.picazarri@uabc.edu.mx>
2018-08-24 19:40:44 -0700

    Practica 1
    Added button 1 & 2

--- Practicas/Assets/Scripts/Pauser.cs
+++ Practicas/Assets/Scripts/Pauser.cs
@@ -1,19 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Pauser : MonoBehaviour {
 	private bool paused = false;
+	public GameObject pausePanel;
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
-			paused = !paused;
+			paused = !paused;	
+			pausePanel.SetActive(paused);
 		}
+		
+	
 
 		if(paused)
 			Time.timeScale = 0;
 		else
 			Time.timeScale = 1;
+	}
+	
+	public void Continue()
+	{
+		Debug.Log("Continuar...");
+		paused = false;
+		pausePanel.SetActive(paused);
+	}
+	
+	public void Reset()
+	{
+		Debug.Log("Reset...");
+		SceneManager.LoadScene(0);
+		
+	}
+	
+	public void Mute()
+	{
+		Debug.Log("Mute...");
 	}
 }

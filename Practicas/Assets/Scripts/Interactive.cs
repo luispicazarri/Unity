f9227c77bda168e607ad5da5a1d8a83bd7a89e28
Luis Antonio Picazarri Quintana <luis.picazarri@uabc.edu.mx>
2018-09-06 19:46:46 -0700

    Practica 2
    Added Interactable Cube

---
+++ Practicas/Assets/Scripts/Interactive.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour {
+	
+	public Color color;
+	
+	private bool isPlayerInside = false; 
+	
+	public GameObject infoPanel; 
+	
+	public int multiplier;
+	
+	private void Start(){
+		infoPanel.SetActive(false);
+	}
+	
+	private void Update(){
+		
+		transform.Rotate(Vector3.up * Time.deltaTime * multiplier);
+		
+		if(Input.GetKeyDown(KeyCode.I) && isPlayerInside){
+			gameObject.GetComponent<Renderer>().material.color = color;
+		}
+	}
+	
+	private void OnTriggerEnter(Collider other){
+		
+		if(other.CompareTag("Player")){
+			
+			infoPanel.SetActive(true);
+			Debug.Log("hola");
+			isPlayerInside = true;
+		}
+	}
+	
+	private void OnTriggerExit(Collider other){
+		if(other.CompareTag("Player")){
+			infoPanel.SetActive(false);
+			isPlayerInside = false;
+		}
+	}
+	
+}

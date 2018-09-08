00d3d6520be9d3979d4a3cc765703c1a95ff4c13
Luis Antonio Picazarri Quintana <luis.picazarri@uabc.edu.mx>
2018-09-07 18:47:42 -0700

    Practica 2
    Completed Practica 2

---
+++ Practicas/Assets/Scripts/StatsPause.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsPause : MonoBehaviour {
+	
+	private bool paused = false;
+	public GameObject pausePanel;
+	public GameObject HUDPanel;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.I))
+		{
+			paused = !paused;	
+			pausePanel.SetActive(paused);
+			HUDPanel.SetActive(!paused);
+			
+			if(paused)
+			Time.timeScale = 0;
+			else
+			Time.timeScale = 1;
+		}
+	}
+}
